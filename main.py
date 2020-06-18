7ef611d6e642f4911098ba6bb8b75b27f2087ab1
tillus23 <till_halfar@yahoo.de>
2020-06-18 12:02:30 +0200

    push it to the limit

--- main.py
+++ main.py
@@ -8,40 +8,51 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+class PingDevice:
+    def __init__(self, name, ip):
+        self.name = name
+        self.ip = ip
+        self.lastSeen = None
+        self.status = None
+        self.lastRefresh = None
+    
+    def ping(self):
+        now = datetime.now()
+        self.lastRefresh = now.strftime("%d.%m.%Y, %H:%M:%S")
 
-pingMaxRes = None
-pingTillRes = None
-lastRefresh = None
+        try:
+            subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower()=="windows" else 'c', self.ip), shell=True)
+            self.status = True
+            self.lastSeen = self.lastRefresh
+        except Exception:
+            self.status = False
+
+pingMaxRes = PingDevice('Max','192.168.0.241')
+pingTillRes = PingDevice('Till','192.168.0.220')
+
+@app.before_first_request
+def pingAllMobiles():
+    pingMaxRes.ping()
+    pingTillRes.ping()
+
 
 @app.route('/')
 def index():
-    if(pingMaxRes == None):
-        pingAllMobiles()
+    return render_template('template.html', pingDevices=[pingTillRes,pingMaxRes])
 
-    return render_template('template.html', pingMaxRes=pingMaxRes, pingTillRes=pingTillRes, timestamp=lastRefresh)
-
-
-def pingAllMobiles():
-    global pingMaxRes
-    global pingTillRes
-    global lastRefresh
-    pingMaxRes = pingOk('192.168.0.241')
-    pingTillRes = pingOk('192.168.0.220')
-    now = datetime.now()
-    lastRefresh = now.strftime("%d.%m.%Y, %H:%M:%S")
 
 def pingOk(sHost):
     try:
         subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower()=="windows" else 'c', sHost), shell=True)
     except Exception:
         return False
-
     return True
 
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=pingAllMobiles, trigger="interval", seconds=60)
-scheduler.start()
-atexit.register(lambda: scheduler.shutdown())
+if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=pingAllMobiles, trigger="interval", seconds=60)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
