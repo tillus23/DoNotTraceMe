44dfefbacb7e0145e8a97c8b342604a3f02279be
tillus23 <till_halfar@yahoo.de>
2020-06-18 11:10:27 +0200

    hehehe

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ import subprocess, platform, os
 from flask import Flask
 from flask import render_template
 from flask import send_from_directory
-
+from datetime import datetime
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+
+pingMaxRes = None
+pingTillRes = None
+lastRefresh = None
+
 @app.route('/')
-def hello_world():
+def index():
+    if(pingMaxRes == None):
+        pingAllMobiles()
+
+    return render_template('template.html', pingMaxRes=pingMaxRes, pingTillRes=pingTillRes, timestamp=lastRefresh)
+
+
+def pingAllMobiles():
+    global pingMaxRes
+    global pingTillRes
+    global lastRefresh
     pingMaxRes = pingOk('192.168.0.241')
     pingTillRes = pingOk('192.168.0.220')
-
-    return render_template('template.html', pingMaxRes=pingMaxRes, pingTillRes=pingTillRes)
+    now = datetime.now()
+    lastRefresh = now.strftime("%d.%m.%Y, %H:%M:%S")
 
 def pingOk(sHost):
     try:
@@ -21,5 +38,10 @@ def pingOk(sHost):
 
     return True
 
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=pingAllMobiles, trigger="interval", seconds=60)
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
 
 app.run(host='0.0.0.0', port=8080, debug=True)
